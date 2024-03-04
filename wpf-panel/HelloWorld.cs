b2a1dd41c10225808d4d12fbf90c79fcd9073667
Tomcat <pine21708@gmail.com>
2024-03-04 23:47:43 +0800

    [Proj] Create wpf-panel project

---
+++ wpf-panel/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wpf_panel
+{
+    public class HelloWorld
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
